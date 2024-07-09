9a577f822098695ad76e0a017a0eea8cdf7babac
cffrankhuangibm <168030073+cffrankhuangibm@users.noreply.github.com>
2024-07-09 12:05:32 -0400

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -54,25 +54,7 @@ class TestAccountService(TestCase):
         """Runs once after each test case"""
         db.session.remove()
 
-    def test_security_headers(self):
-        """It should return security headers"""
-        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        headers = {
-            'X-Frame-Options': 'SAMEORIGIN',
-            'X-Content-Type-Options': 'nosniff',
-            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
-            'Referrer-Policy': 'strict-origin-when-cross-origin'
-        }
-        for key, value in headers.items():
-            self.assertEqual(response.headers.get(key), value)
-
-    def test_cors_security(self):
-        """It should return a CORS header"""
-        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Check for the CORS header
-        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+   
 
     ######################################################################
     #  H E L P E R   M E T H O D S
@@ -147,12 +129,12 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    def test_get_account_not_found(self):
+    def test_get_account_not_found(self):                                          #
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_get_account(self):
+    def test_get_account(self):                                                      #
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.get(
@@ -162,7 +144,7 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-    def test_get_account_list(self):
+    def test_get_account_list(self):                                                 #
         """It should Get a list of Accounts"""
         self._create_accounts(5)
         resp = self.client.get(BASE_URL)
@@ -170,7 +152,7 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
-    def test_update_account(self):
+    def test_update_account(self):                                                  #
         """It should Update an existing Account"""
         # create an Account to update
         test_account = AccountFactory()
@@ -185,7 +167,7 @@ class TestAccountService(TestCase):
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
 
-    def test_delete_account(self):
+    def test_delete_account(self):                                             #
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
